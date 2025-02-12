94e60d3e385c6dd5af4a3f45ede32b4edbfd503f
sahara <mrjung72@gmail.com>
2025-02-12 18:02:02 +0900

    회원목록 조회기능 추가

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,17 +6,22 @@ const User = {
       "INSERT INTO users (email, password) VALUES (?, ?)",
       [email, password]
     );
-    return result.insertId; // 생성된 사용자 ID 반환
+    return result.insertId;
   },
 
   findByEmail: async (email) => {
     const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [email.email]);
-    return rows[0]; // 첫 번째 결과 반환
+    return rows[0];
   },
 
   findById: async (id) => {
     const [rows] = await pool.query("SELECT id, email, createdAt FROM users WHERE id = ?", [id]);
-    return rows[0]; // 첫 번째 결과 반환
+    return rows[0];
+  },
+
+  findAll: async () => {
+    const [rows] = await pool.query("SELECT id, email, createdAt FROM users");
+    return rows;
   }
 };
 
