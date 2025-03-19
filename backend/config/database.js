ead6ea1627d9edab2b8c2ac6671a4dbbdbc3fda9
sahara <mrjung72@gmail.com>
2025-03-19 20:45:16 +0900

    비밀번호변경경

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("userinfodb", "sahara", "7803", {
+const sequelize = new Sequelize("userinfodb", "sahara", "1111", {
   host: "localhost",
   dialect: "mariadb",  // MariaDB 사용
 });
